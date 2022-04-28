1637b346609d2d598eb0c2d5812e5d8eb6fb9edd
David Dane <danedavid46@gmail.com>
2022-04-28 20:39:35 +0300

    Install mongoose, validate query params

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import { json } from 'body-parser';
-import { suggestionRouter } from './routes/city';
+import { cityRouter } from './routes/city';
 
 const app = express();
 app.use(json());
-app.use(suggestionRouter);
+app.use(cityRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- src/routes/city.ts
+++ src/routes/city.ts
@@ -1,9 +1,22 @@
 import express, {Request, Response} from 'express';
+import { query, validationResult} from 'express-validator';
 
 const router = express.Router();
 
-router.get('/suggestion', [], (req: Request, res: Response) => {
-    return res.send('test response');
+const queryStringValidators = [
+    query('q').isLength({min: 1}),
+    query('latitude').optional().isFloat(),
+    query('longitude').optional().isFloat(),
+    query('radius').optional().isInt({min: 0}),
+    query('sort').optional().isIn(['name', 'distance']),
+];
+
+router.get('/suggestions', queryStringValidators, (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    return res.json({suggestions: ['Paris', 'London', 'New York']});
 });
 
-export { router as suggestionRouter };
+export { router as cityRouter };
