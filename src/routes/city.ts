d2ec2b12514a3357d5674738c4e7f11abec62898
David Dane <danedavid46@gmail.com>
2022-04-30 13:34:34 +0300

    Fetch cities by location

--- src/routes/city.ts
+++ src/routes/city.ts
@@ -1,22 +1,78 @@
 import express, { Request, Response } from 'express';
 import { query, validationResult } from 'express-validator';
+import mongoose, { Schema } from 'mongoose';
 
 const router = express.Router();
 
+interface IQuery {
+    q: String,
+    latitude: number,
+    longitude: number,
+    radius: number,
+    sort: number,
+}
+
+const CitySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true,
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        },
+    }
+});
+
+CitySchema.index({ location: '2dsphere' });
+
+const CityModel = mongoose.model('cities', CitySchema);
+
 const queryStringValidators = [
-    query('q').isLength({ min: 1 }),
-    query('latitude').optional().isFloat(),
-    query('longitude').optional().isFloat(),
-    query('radius').optional().isInt({ min: 0 }),
+    query('q').isString().isLength({ min: 1 }),
+    query('latitude').isFloat({ min: -90, max: 90 }),
+    query('longitude').isFloat({ min: -180, max: 180 }),
+    query('radius').isNumeric(),
     query('sort').optional().isIn(['name', 'distance']),
 ];
 
 router.get('/suggestions', queryStringValidators, (req: Request, res: Response) => {
     const errors = validationResult(req);
+    const { q, longitude, latitude, radius, sort } = req.query as unknown as IQuery;
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    return res.json({ suggestions: ['Paris', 'London', 'New York'] });
+    // CityModel
+    //     .aggregate()
+    //     .near({
+    //         near: [longitude, latitude],
+    //         distanceField: 'distance',
+    //         query: { name: { $regex: `^${q}`, $options: 'i' } },
+    //     })
+    //     .then((cities) => res.json({ suggestions: cities }))
+    //     .catch((error) => res.status(500).json({ error }));
+    CityModel
+        .find({
+            name: new RegExp(`^${q}`, 'i'),
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: radius,
+                },
+            },
+        })
+        .select('name location distance')
+        .then((cities) => res.json({ suggestions: cities }))
+        .catch((error) => res.status(500).json({ error }));
 });
 
 export { router as cityRouter };
