68e1986acfcaff255a69ebacd17ac464c644cf2e
David Dane <danedavid46@gmail.com>
2022-05-01 17:05:28 +0300

    Add rate limiting

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import express from 'express';
 import { json } from 'body-parser';
 import { cityRouter } from './routes/city';
 import { connect } from './utils/mongoUtils';
+import rateLimit from 'express-rate-limit'
 
 const app = express();
 
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minutes
+    max: 5,
+    standardHeaders: true,
+})
+
 app.use(json());
+app.use(limiter);
 app.use(cityRouter);
 
 connect();
